b44e0bd267fb3c7c9d096d693afc17d2e9410be6
Cele1979 <106399254+Cele1979@users.noreply.github.com>
2022-08-14 14:05:21 -0300

    Ejercicio 7

---
+++ Ejercicio_7.py
@@ -0,0 +1,8 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+Historial3= (9530,4120,4580,1500,890,7516,426)
+print(Historial3)
+(9530, 4120, 4580, 1500, 890, 7516, 426)
+Lennon=sum(Historial3)/len(Historial3)
+print(Lennon)
+4080.285714285714
